d812939a52e5a23efc028520747757f297aa62f9
graMstrs <gra.tavera@gmail.com>
2025-09-03 17:58:25 -0600

    250903_Sec3.12

---
+++ ReactDeCeroAExperto/01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,9 @@
+
+
+const firstName = 'Gerardo';
+const lastName = 'Tavera';
+
+const containsLetterR = lastName.includes('r');
+
+
+console.log({ containsLetterR });
---
+++ ReactDeCeroAExperto/01-reforzamiento/src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+import './bases/01-const-let';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+   <h1> Hola mundo</h1>
+  </div>
+`;
+
+
