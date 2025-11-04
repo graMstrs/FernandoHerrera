52f70fd32e8275c670d16ea1ff553444cc748e09
graMstrs <gra.tavera@gmail.com>
2025-11-03 19:20:38 -0600

    251103_React_8.102

---
+++ ReactDeCeroAExperto/03-gifs-app/src/gifs/api/giphy.api.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, test } from "vitest";
+import { giphyApi } from "./giphy.api";
+
+describe("giphyApi", () => {
+  test("should be configured correclty", () => {
+    const params = giphyApi.defaults.params;
+    console.log(params);
+
+    expect(giphyApi.defaults.baseURL).toBe("https://api.giphy.com/v1/gifs");
+    expect(params.lang).toBe("es");
+    expect(params.api_key).toBe(import.meta.env.VITE_GIPHY_API_KEY);
+  });
+});
